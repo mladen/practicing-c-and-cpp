674c135209ae9931bf7fc719ffcc7965d6a2b6a7
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-10-27 00:34:47 +0200

    Adds chess_board.c for printing a chess board with pieces

---
+++ chess_board.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <locale.h>
+#include <wchar.h>
+
+void print_chess_board()
+{
+    // Unicode characters for the pieces
+    // TODO: Maybe use Unicode characters for the pieces?
+    const wchar_t *board[8][8] = {
+        {L"♖", L"♘", L"♗", L"♕", L"♔", L"♗", L"♘", L"♖"},
+        {L"♙", L"♙", L"♙", L"♙", L"♙", L"♙", L"♙", L"♙"},
+        {L" ", L" ", L" ", L" ", L" ", L" ", L" ", L" "},
+        {L" ", L" ", L" ", L" ", L" ", L" ", L" ", L" "},
+        {L" ", L" ", L" ", L" ", L" ", L" ", L" ", L" "},
+        {L" ", L" ", L" ", L" ", L" ", L" ", L" ", L" "},
+        {L"♟", L"♟", L"♟", L"♟", L"♟", L"♟", L"♟", L"♟"},
+        {L"♜", L"♞", L"♝", L"♛", L"♚", L"♝", L"♞", L"♜"}};
+
+    // Print the board
+    for (int row = 0; row < 8; row++)
+    {
+        for (int col = 0; col < 8; col++)
+        {
+            wprintf(L"%ls ", board[row][col]);
+        }
+        wprintf(L"\n");
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, ""); // Set locale for Unicode support
+    print_chess_board();
+    return 0;
+}
